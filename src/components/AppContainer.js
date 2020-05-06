2f07aaad3198e3ea8b02557e0483ef29f380d65d
Jerry Jones <jerryjfernandes@gmail.com>
2020-05-06 13:07:20 -0300

    Lógica de integração API: get

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
 export class AppContainer extends Component {
+  state = {
+    produtos:[]
+  }
+
+  componentDidMount = () => {
+    this.buscaProdutos();
+  }
+
+  buscaProdutos = async () => {
+    try {
+      const resposta = await axios.get('https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products');
+      this.setState({produtos: resposta.data.products})
+    } catch(error) {
+      console.log("ERROR")
+    }
+    
+  } 
+
+  mostraProdutos = () => {
+    console.log(this.state.produtos)
+  }
+
   render() {
     return (
       <div>
-
+          <button onClick={this.mostraProdutos}>Console</button>
       </div>
     )
   }
