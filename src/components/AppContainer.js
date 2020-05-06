8c183feb60fa82a9df48e98e3d27aeb68602f0a2
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-05-06 17:38:54 -0300

    inclusão da lógica de filtro de categoria e menu categorias agora recebe da api

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -37,26 +37,35 @@ grid-area: bottomPage;
 background-color: #202020;
 `
 
-
 export class AppContainer extends Component {
   state = {
     produtos:[],
-    perfilDoUsuario: ''
+    perfilDoUsuario: '',
+    produtosFiltradosPorCategoria: []
   }
 
-  componentDidMount = () => {
-    this.buscaProdutos();
-  }
-
-  buscaProdutos = async () => {
-    try {
-      const resposta = await axios.get('https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products');
-      this.setState({produtos: resposta.data.products})
-    } catch(error) {
-      console.log("ERROR")
+    componentDidMount = () => {
+      this.buscaProdutos();
     }
-    
-  } 
+
+    buscaProdutos = async () => {
+      try {
+        const resposta = await axios.get('https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products');
+        this.setState({produtos: resposta.data.products})
+        console.log(this.state.produtos[1].category)
+      } catch(error) {
+        console.log(error)
+      }
+    }
+
+  filtroCategoria = (nomeDaCategoria) => {
+    const categoriaFiltrada = this.state.produtos.filter(produto => {
+      if(produto.category === nomeDaCategoria) {
+        return true
+      }
+    })
+    this.setState({produtosFiltradosPorCategoria: categoriaFiltrada})
+  }
 
   perfilComprador = () => {
     this.setState({perfilDoUsuario: 'comprador'})
@@ -69,11 +78,11 @@ export class AppContainer extends Component {
   render() {
 
     //Popula a lista de categorias antes da integração com a API
-    let listaCategorias = []
-    for(let i = 0; i < 10; i++) {
-      let categoria = <li>categoria {i}</li>
-      listaCategorias.push(categoria)
-    }
+    //let listaCategorias = []
+    //for(let i = 0; i < 10; i++) {
+    //  let categoria = <li>categoria {i}</li>
+    //  listaCategorias.push(categoria)
+    //}
 
     let telaDoComprador
     let telaDoVendedor
@@ -87,7 +96,7 @@ export class AppContainer extends Component {
         break
       default:
         telaDeEscolha = '<Componente de Escolha />'
-    }
+    } 
 
     return (
       <PageContainer>
@@ -104,7 +113,13 @@ export class AppContainer extends Component {
         </MainContent>
         <MenuContent>
           <MenuCategorias
-            categoria={listaCategorias}
+            categoria={this.state.produtos.map((produto) => {
+              return  <li>
+                        <a href='#' onClick={() => this.filtroCategoria(produto.category)}>
+                          {produto.category}
+                        </a>
+                      </li>
+            })}
           />
         </MenuContent>
         <Footer>
