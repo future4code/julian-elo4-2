3fd6ffc5a12f45c198d78a76672ae2d3787d7274
Jerry Jones <jerryjfernandes@gmail.com>
2020-05-08 11:52:35 -0300

    Alterações layout página inicial e página de cadastro

--- src/components/TelaInicial.js
+++ src/components/TelaInicial.js
@@ -11,11 +11,19 @@ background-color: #F5F5F5;
 `
 const DivCentralizada = styled.div`
 background-color: #f2970b;
-width: 50vw;
-height: 50vh;
+width: 100vw;
+height: 110vh;
 margin: auto;
 padding-top: 5vh;
+display: flex;
+flex-direction: column;
+padding-top: 8rem;
 `
+const DivBotoes = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 const ButtonComprar = styled.div`
 margin: 5vw;
 display: inline;
@@ -35,16 +43,18 @@ export class TelaInicial extends Component {
                 <DivCentralizada>
                     <h1>Bem vindo!</h1>
                     <h3>O que você quer fazer?</h3>
-                    <ButtonComprar>                
-                        <Button variant="contained" color="primary" onClick={this.props.buttonComprar}>
-                            COMPRAR
-                        </Button>
-                    </ButtonComprar>
-                    <ButtonVender>
-                        <Button variant="contained" color="secondary" onClick={this.props.buttonVender}>
-                            VENDER
-                        </Button>
-                    </ButtonVender>
+                    <DivBotoes>
+                        <ButtonComprar>                
+                            <Button variant="contained" color="primary" onClick={this.props.buttonComprar}>
+                                COMPRAR
+                            </Button>
+                        </ButtonComprar>
+                        <ButtonVender>
+                            <Button variant="contained" color="secondary" onClick={this.props.buttonVender}>
+                                VENDER
+                            </Button>
+                        </ButtonVender>
+                    </DivBotoes>
                 </DivCentralizada>                
             </Container>
         )
