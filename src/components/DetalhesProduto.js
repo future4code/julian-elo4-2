5a1919609b64da0e34a6cea4f246a8cc666f581f
Jerry Jones <jerryjfernandes@gmail.com>
2020-05-08 15:04:42 -0300

    Lógica para exibir detalhes dos produtos 4

--- src/components/DetalhesProduto.js
+++ src/components/DetalhesProduto.js
@@ -50,19 +50,7 @@ export default class DetalhesProduto extends React.Component {
         <DetalhesProdutoParteDois>
           <p><strong>Categoria: </strong>categoria A</p>
           <h3>Descrição:</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            In elit quam, finibus non nisi vel, ultricies finibus tellus. 
-            Mauris ut libero non magna eleifend consequat ut eget tellus. 
-            Fusce sed velit a massa aliquet ornare. Maecenas a suscipit nibh. 
-            Duis placerat velit at diam ultrices gravida. 
-            Fusce laoreet dui a neque egestas pretium. 
-            Duis risus purus, ultrices eu tincidunt eget, scelerisque sit amet risus. 
-            Donec non nunc at urna vehicula volutpat ac eget massa. Morbi nec egestas turpis. 
-            Nulla porta urna vel lacus ultrices faucibus. 
-            Nam aliquet metus mi, sit amet volutpat tellus malesuada at. 
-            Vivamus eget venenatis nisi, id fermentum dui. 
-            Duis rutrum nisl ut velit interdum vehicula. 
-            In hac habitasse platea dictumst.
+          <p>Descrição do Produto
           </p>
         </DetalhesProdutoParteDois>
       </ContainerPrincipal>
